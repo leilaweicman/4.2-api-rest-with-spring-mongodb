9889ae35bed0cc2e144763c98b1cb9f109071735
leilaweicman <leilaweic@hotmail.com>
2025-12-10 16:04:50 +0100

    feat: add OrderController with createOrder

---
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/controller/OrderController.java
@@ -0,0 +1,29 @@
+package cat.itacademy.s04.t02.n03.fruit.controller;
+
+import cat.itacademy.s04.t02.n03.fruit.dto.OrderCreateRequest;
+import cat.itacademy.s04.t02.n03.fruit.dto.OrderResponse;
+import cat.itacademy.s04.t02.n03.fruit.service.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> createOrder(@Valid @RequestBody OrderCreateRequest request) {
+        OrderResponse response = orderService.createOrder(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
