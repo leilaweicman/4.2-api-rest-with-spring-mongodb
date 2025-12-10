c14c6a6f7a64c43d1ee685005bb47a690ad83258
leilaweicman <leilaweic@hotmail.com>
2025-12-10 12:20:12 +0100

    refactor: apply Lombok to Order and OrderItem entities

--- src/main/java/cat/itacademy/s04/t02/n03/fruit/model/Order.java
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/model/Order.java
@@ -1,5 +1,6 @@
 package cat.itacademy.s04.t02.n03.fruit.model;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -7,33 +8,18 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Document(collection = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Order {
 
     @Id
     private String id;
+
     private String clientName;
     private LocalDate deliveryDate;
     private List<OrderItem> items;
-
-    public Order() {}
-
-    public Order(String id, String clientName, LocalDate deliveryDate, List<OrderItem> items) {
-        this.id = id;
-        this.clientName = clientName;
-        this.deliveryDate = deliveryDate;
-        this.items = items;
-    }
-
-    public Order(String clientName, LocalDate deliveryDate, List<OrderItem> items) {
-        this.clientName = clientName;
-        this.deliveryDate = deliveryDate;
-        this.items = items;
-    }
-
-    // getters
-    public String getId() { return id; }
-    public String getClientName() { return clientName; }
-    public LocalDate getDeliveryDate() { return deliveryDate; }
-    public List<OrderItem> getItems() { return items; }
 }
 
--- src/main/java/cat/itacademy/s04/t02/n03/fruit/model/OrderItem.java
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/model/OrderItem.java
@@ -1,18 +1,15 @@
 package cat.itacademy.s04.t02.n03.fruit.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderItem {
 
     private String fruitName;
     private int quantityInKilos;
-
-    public OrderItem() {}
-
-    public OrderItem(String fruitName, int quantityInKilos) {
-        this.fruitName = fruitName;
-        this.quantityInKilos = quantityInKilos;
-    }
-
-    public String getFruitName() { return fruitName; }
-    public int getQuantityInKilos() { return quantityInKilos; }
 }
 
