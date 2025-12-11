c2d9d11fd81b86105af2717af1d3e94f6efe9109
leilaweicman <leilaweic@hotmail.com>
2025-12-11 10:52:12 +0100

    test: add method for GET /orders in Controller

--- src/main/java/cat/itacademy/s04/t02/n03/fruit/controller/OrderController.java
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/controller/OrderController.java
@@ -6,10 +6,9 @@ import cat.itacademy.s04.t02.n03.fruit.service.OrderService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
@@ -25,5 +24,10 @@ public class OrderController {
         OrderResponse response = orderService.createOrder(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> getAllOrders() {
+        return ResponseEntity.ok(orderService.getAllOrders());
     }
 }
